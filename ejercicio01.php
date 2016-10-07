cd0251680f1a4c1a9ffb41dfb2c1158c14240975
Jorge Casals Alonso <jorgecasalsalonso@gmail.com>
2016-10-07 16:28:11 +0200

    index y limpieza

--- ejercicio01.php
+++ ejercicio01.php
@@ -1,13 +1,25 @@
 <html>
-	<head>
-		<title>Ejercicio 01</title>
-	</head>
-	<body>
-		<?php
-			$array = array("Patata", 2, "prueba", 4, "algo");
-			for ($i = 0; $i < count($array); $i++){
-				echo "<p>" . $array[$i] . "</p>";
-			}
-		?>
-	</body>
+    <head>
+        <title>Ejercicio 01</title>
+    </head>
+    <body>
+        <?php
+        $array1 = array("Pepe", "Juan", "Alberto", "Carlos", "Mariano");
+        echo "<table><tr>";
+        for ($i = 0; $i < count($array1); $i++) {
+            echo "<td>" . $array1[$i] . "</td>";
+        }
+        echo "</tr></table>";
+        $array2[] = "Pepe";
+        $array2[] = "Juan";
+        $array2[] = "Alberto";
+        $array2[] = "Carlos";
+        $array2[] = "Mariano";
+        echo "<table>";
+        foreach ($array1 as $position => $element) {
+            echo "<tr><th>" . $position . "</th><td>" . $element . '</td></tr>';
+        }
+        echo "</table>";
+        ?>
+    </body>
 </html>
